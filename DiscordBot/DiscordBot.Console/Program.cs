22b276b60bfa561a34975c844e4ee5e4cb984e09
Miguel Molina Jr <molim134@gmail.com>
2019-10-07 23:05:13 -0400

    start the discord bot that will interact with the stats api

---
+++ DiscordBot/DiscordBot.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DiscordBot.Console
+{
+    using DiscordBot.Core;
+
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args).ConfigureServices(services =>
+            {
+                services.AddLogging().AddHostedService<DiscordBot>();
+            });
+        }
+    }
+}
---
+++ DiscordBot/DiscordBot.Core/DiscordBot.cs
@@ -0,0 +1,45 @@
+﻿namespace DiscordBot.Core
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Discord;
+
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Hosting;
+
+    public class DiscordBot : IHostedService
+    {
+        private readonly IConfiguration configuration;
+
+        private DiscordClient discordClient;
+
+        public DiscordBot(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            string discordToken = GetDiscordToken();
+
+            discordClient = new DiscordClient();
+            await discordClient.Connect(discordToken, TokenType.Bot);
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            if (discordClient is null)
+                return;
+
+            await discordClient?.Disconnect();
+            discordClient?.Dispose();
+            discordClient = null;
+        }
+
+        private string GetDiscordToken()
+        {
+            return configuration.GetSection("AppSettings")["Token"];
+        }
+    }
+}
