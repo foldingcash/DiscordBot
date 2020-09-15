29d54559b6a83e3b29611488b7331d7f16986791
Miguel Molina Jr <molim134@gmail.com>
2020-09-14 21:10:04 -0400

    Update install scripts to make easy to cycle builds

--- DiscordBot/DiscordBot.Console/install_service.ps1
+++ DiscordBot/DiscordBot.Console/install_service.ps1
@@ -1,10 +1,18 @@
-$params = @{
-  Name = "FoldingCashDiscordBot"
-  DisplayName = "Folding Cash Discord Bot"
-  Description = "This is a bot for FoldingCash's Discord server."
-  BinaryPathName = "dotnet C:\DiscordBot\DiscordBot.Console.dll --environment=production"
-  StartupType = "Automatic"
-}
-New-Service @params
+$InstallPath = 'C:\DiscordBot'
+$BuildPath = 'C:\source\foldingcash\DiscordBot\DiscordBot\DiscordBot.Console\bin\Release\netcoreapp3.1'
 
-Start-Service -Name "FoldingCashDiscordBot"
+'InstallPath: ' + $InstallPath
+'BuildPath: ' + $BuildPath
+
+'Uninstalling windows service'
+.\uninstall_windows_service.ps1
+
+'Removing build'
+Remove-Item -Path $InstallPath -Recurse
+New-Item -Path $InstallPath -ItemType Directory
+Copy-Item -Path $BuildPath -Destination $InstallPath -Recurse
+'Copied build'
+
+'Installing Windows Service'
+.\install_windows_service.ps1
+'Finished Install Windows Service'
